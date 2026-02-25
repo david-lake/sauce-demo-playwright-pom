f45a8831ccf290d52fc9a105c42c8e968dc88a0c
David Lake <55196694+david-lake@users.noreply.github.com>
2026-02-25 19:08:39 +0000

    test: wrap auth.spec tests in test.describe block (#8)
    * ci: remove --project flag to fix deploy test run error
    
    * test: wrap auth.spec tests in test.describe block
    
    * Fix tests

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -1,15 +1,17 @@
 import { test } from '@fixtures/app.fixture';
 import { users } from '@data/users';
 
-test('standard user can login', async ({ app }) => {
-  await app.login.visit();
-  await app.login.loginAs(users.standard);
-  await app.products.assertLoaded();
-});
+test.describe('Authentication', () => {
+  test('standard user can login', async ({ app }) => {
+    await app.login.visit();
+    await app.login.loginAs(users.standard);
+    await app.products.assertLoaded();
+  });
 
-test('locked out user can not login', async ({ app }) => {
-  await app.login.visit();
-  await app.login.loginAs(users.locked);
-  await app.login.assertLoaded();
-  await app.login.assertError();
+  test('locked out user can not login', async ({ app }) => {
+    await app.login.visit();
+    await app.login.loginAs(users.locked);
+    await app.login.assertLoaded();
+    await app.login.assertError();
+  });
 });
